b8343854da702d15cf0c1a8b6e16c5e2e826d152
joyerreynolds18 <reynoldsvirginia3@gmail.com>
2025-10-29 19:44:50 -0400

    Implement distance conversion in fitness app
    Added distance conversion from feet to meters and printed the result.

--- Unit1/Lesson1_4/Lab_Operators.playground/Pages/4. App Exercise - Counting.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_4/Lab_Operators.playground/Pages/4. App Exercise - Counting.xcplaygroundpage/Contents.swift
@@ -8,6 +8,7 @@
 
 
 /*:
+
  In addition to tracking steps, your fitness tracking app tracks distance traveled. Create a variable `distance` of type `Double` and set it equal to 50. This will represent the user having traveled 50 feet.
  
  You decide, however, to display the distance in meters. 1 meter is approximately equal to 3 feet. Use a compound assignment operator to convert `distance` to meters. Print the result.
@@ -17,3 +18,12 @@
 /*:
 [Previous](@previous)  |  page 4 of 8  |  [Next: Exercise - Order of Operations](@next)
  */
+
+
+var steps = 0 
+steps += 1
+print (steps)
+
+var distance = 50.00
+var distanceInMeters = distance/3
+print(distanceInMeters)
