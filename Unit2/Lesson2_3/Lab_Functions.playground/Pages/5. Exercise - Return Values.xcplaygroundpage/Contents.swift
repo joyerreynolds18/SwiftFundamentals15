ebed75d4b1790c2ec00470a6ddd7f3aab70f7514
joyerreynolds18 <reynoldsvirginia3@gmail.com>
2025-11-16 19:33:19 -0500

    Add greeting and multiplyAndAdd functions

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -4,9 +4,25 @@
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Sophie" the return value might be "Hi, Sophie! How are you?" Use the function and print the result.
  */
 
+func greeting(name: String) -> String {
+    return "Hi, \(name)! How are you?"
+}
+
+// Use the function
+let message = greeting(name: "Sophie")
+print(message)
+
+print(greeting(name: "Joyer"))
 
 //:  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
+ func multiplyAndAdd(_ a: Int, _ b: Int) -> Int {
+      return ( a * b) + 2
+ }
 
+// Use the function
+let result = multiplyAndAdd(5,4)
+print(result)
+                           
 
 /*:
 [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
